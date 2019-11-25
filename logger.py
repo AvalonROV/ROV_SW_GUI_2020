a7b237fc99a0fca0eb32eb3ca577fdf18fc10acd
Sam Maxwell <maxwell.g.sam@gmail.com>
2019-11-25 00:56:56 +0000

    - Completed development of a basic logger class for generating log files for the ROV.

--- logger.py
+++ logger.py
@@ -13,17 +13,13 @@ VERSION DATE: N/A
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 #MODULES
 #-----------------------------------------------------------------------------------------------------------------------------------------------
-import os
+from os import path
 import datetime
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------
-#DEFINITIONS
-#-----------------------------------------------------------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 #CLASSES
 #-----------------------------------------------------------------------------------------------------------------------------------------------
-class logFile():
+class LogFile():
     """
     PURPOSE
     
@@ -31,7 +27,10 @@ class logFile():
     in a log file. Log files are created as a basic text file.
     """
     #DEFINITIONS
-    fileAddress = ""
+    __fileAddress = ""                    #The address of the logging file
+    timeFormat = "%Y%m%dT%H%M%SZ"       #The format of the timestamp to be placed in the logging file name and in the logging file entries
+    seperator = ","                     #The character used to seperate terms in a log entry
+    delimiter = "\n"                    #The character used to create a new line
     
     def __init__(self, fileName, fileFolder = None):
         """
@@ -49,8 +48,9 @@ class logFile():
         
         NONE
         """
-        self.__createFileAddress(fileName, fileFolder)      #Generating the file address of the log file
-        
+        self.__createFileAddress(fileName, fileFolder)
+        self.__checkFileAddress()
+        self.__createLogFile()
         
     def __createFileAddress(self, fileName, fileFolder = None):
         """
@@ -69,9 +69,13 @@ class logFile():
         """
         #If no file folder is given, create the file in the folder the code is running. Otherwise, create it in the specified folder.
         if fileFolder != None:
-            self.fileAddress = fileFolder + "\\" + fileName + ".txt"
+            self.__fileAddress = fileFolder + "\\" + fileName + ".txt"
         else:
-            self.fileAddress = fileName + ".txt"
+            self.__fileAddress = fileName + ".txt"
+        
+        #Adding a timestamp to the file address
+        currentTime = datetime.datetime.now()
+        self.__fileAddress = currentTime.strftime(self.timeFormat) + " " + self.__fileAddress
     
     def __checkFileAddress(self):
         """
@@ -85,6 +89,74 @@ class logFile():
         
         RETURNS
         
-        - bool = True if the file passes tests, False if it does not.
+        NONE
         """
-        pass
+        #Checking if the file exists
+        if path.exists(self.__fileAddress):
+            raise IOError("A file at {} already exists".format(self.__fileAddress))
+    
+    def __createLogFile(self):
+        """
+        PURPOSE
+        
+        Creates the log file at the given address.
+        
+        INPUT
+        
+        NONE
+        
+        OUTPUT
+        
+        NONE
+        """
+        #Creating the log file
+        self.addLogEntry("Logging Started")
+    
+    def addLogEntry(self, logEntryText):
+        """
+        PURPOSE
+        
+        Adds a single log entry to the log file with the given text.
+        
+        INPUT
+        
+        logEntryText = The text to be recorded in the log entry as a string.
+        
+        OUTPUT
+        
+        logAdded (Bool) = Returns whether the log entry was successfully added or not.
+        """
+        #Creating the success tracking boolean
+        logAdded = False
+        
+        #Building the log entry
+        currentTime = datetime.datetime.now()
+        logEntry = currentTime.strftime(self.timeFormat) + self.seperator + logEntryText + self.delimiter
+        
+        #Opening the log file and writing the entry
+        try:
+            logFile = open(self.__fileAddress, "a")
+            logFile.write(logEntry)
+            logFile.close()
+            logAdded = True
+            
+        except IOError:
+            print("Failed to write to log file, ensure it is not open in another program or create a new file")
+        
+        return logAdded
+    
+    def getFileAddress(self):
+        """
+        PURPOSE
+        
+        Returns the file address of the log file.
+        
+        INPUT
+        
+        NONE
+        
+        OUTPUT
+        
+        fileAddress (string) = The address of the logging file.
+        """
+        return self.__fileAddress
